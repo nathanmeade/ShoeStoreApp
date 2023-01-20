e337fead22fa7544341e747e808cf20d1c3cf43b
nathanmeade <nathan.r.meade@gmail.com>
2023-01-19 20:35:52 -0500

    InstructionFragment moved to a new instruction package.

--- starter/app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
@@ -1,10 +1,11 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.ui.instruction
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.udacity.shoestore.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
