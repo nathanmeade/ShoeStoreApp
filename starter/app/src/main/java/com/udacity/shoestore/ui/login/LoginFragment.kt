47c3d9f5db5c744860b906f67839919c14a1c2ae
nathanmeade <nathan.r.meade@gmail.com>
2023-01-31 13:21:36 -0500

    DataBinding added to LoginFragment so that the login and register buttons can be properly bound to their associated actions

--- starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -1,19 +1,37 @@
 package com.udacity.shoestore.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false)
+        binding.loginButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            findNavController(this).navigate(action)
+        }
+        binding.registerButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            findNavController(this).navigate(action)
+        }
+        return binding.root
     }
 
 }
