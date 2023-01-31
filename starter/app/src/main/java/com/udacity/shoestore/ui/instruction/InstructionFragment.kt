ea288ef54266184d7622a7a02b70361131424fc0
nathanmeade <nathan.r.meade@gmail.com>
2023-01-31 13:34:57 -0500

    DataBinding added to InstructionFragment to properly bind the next button to it's associated action

--- starter/app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/instruction/InstructionFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
 class InstructionFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_instruction,
+            container,
+            false)
+        binding.nextButton.setOnClickListener {
+            val action = InstructionFragmentDirections.actionInstructionFragmentToShoeListFragment()
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+        return binding.root
     }
 
 }
