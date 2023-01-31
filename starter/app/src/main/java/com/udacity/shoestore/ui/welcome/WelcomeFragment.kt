9d5d5eaf8ade58c5e7dfdec80ea727961dbc978e
nathanmeade <nathan.r.meade@gmail.com>
2023-01-31 13:28:38 -0500

    DataBinding added to WelcomeFragment to properly bind the next button to it's associated action

--- starter/app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import com.udacity.shoestore.ui.login.LoginFragmentDirections
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_welcome,
+            container,
+            false)
+        binding.nextButton.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment()
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+        return binding.root
     }
     
 }
