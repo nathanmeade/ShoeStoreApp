588d18a6489ba77c22deab789e8f4908b1e4f439
nathanmeade <nathan.r.meade@gmail.com>
2023-01-31 15:13:52 -0500

    Fragment labels changed and shoe list menu updated to reflect the current recommended implementation

--- starter/app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListFragment.kt
@@ -2,7 +2,10 @@ package com.udacity.shoestore.ui.shoelist
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
@@ -16,12 +19,20 @@ class ShoeListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_shoe_list, container, false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.overflow_menu, menu)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.overflow_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return NavigationUI.onNavDestinationSelected(menuItem, requireView().findNavController())
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        super.onViewCreated(view, savedInstanceState)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
-    }
 }
