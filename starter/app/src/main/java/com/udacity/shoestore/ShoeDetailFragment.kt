5d6185409b76a721358ebe5e943ba021620fb4f1
nathanmeade <nathan.r.meade@gmail.com>
2023-01-31 15:57:41 -0500

    Save and cancel button functionality now implemented for ShoeDetailFragment

--- starter/app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.data.model.Shoe
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.ui.ShoeViewModel
 
 class ShoeDetailFragment : Fragment() {
+
+    private lateinit var viewModel: ShoeViewModel
+
+    private lateinit var binding: FragmentShoeDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_shoe_detail,
+            container,
+            false)
+        viewModel = ViewModelProvider(this)[ShoeViewModel::class.java]
+        binding.shoeViewModel = viewModel
+        binding.cancelButton.setOnClickListener {
+            val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
+            findNavController().navigate(action)
+        }
+        binding.saveButton.setOnClickListener {
+            val shoe = Shoe(binding.nameField.text.toString(), binding.sizeField.text.toString().toDouble(), binding.companyField.text.toString(), binding.descriptionField.text.toString())
+            viewModel.addShoe(shoe)
+            val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
+            findNavController().navigate(action)
+        }
+        return binding.root
     }
 
 }
